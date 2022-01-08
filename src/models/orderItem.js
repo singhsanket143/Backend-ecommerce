1558d62b0daec7571775cf453c32239fae26c10a
singhsanket143 <singhsanket143@gmail.com>
2022-01-08 11:33:46 +0530

    Added crud for order items

---
+++ src/models/orderItem.js
@@ -0,0 +1,54 @@
+const sqlConnection = require("../services/sqlConnection");
+
+function addOrderItem(data, cb) {
+    var sql = ` INSERT INTO OrderItems
+                (OrderID, ProductID, Quantity, CreatedAt, UpdatedAt)
+                VALUES (? , ? , ? , now(), now())`;
+    var values = [];
+    values.push(data.orderId);
+    values.push(data.productId);
+    values.push(data.quanitity);
+    sqlConnection.executeQuery(sql, values, function(err, result) {
+        cb(err, result);
+    });
+}
+
+function editOrderItem(data, cb) {
+    var sql = ` UPDATE OrderItems SET 
+                Quantity = ? , UpdatedAt = now() WHERE
+                OrderId = ? AND ProductID = ?`;
+
+    var values = [];
+    values.push(data.quanitity);
+    values.push(data.orderId);
+    values.push(data.productId);
+    sqlConnection.executeQuery(sql, values, function(err, result) {
+        cb(err, result);
+    });
+}
+
+function deleteOrderItem(data, cb) {
+    var sql = ` DELETE FROM OrderItems WHERE
+                OrderId = ? AND ProductID = ?`;
+
+    var values = [];
+    values.push(data.orderId);
+    values.push(data.productId);
+    sqlConnection.executeQuery(sql, values, function(err, result) {
+        cb(err, result);
+    });
+}
+
+function getOrderItems(data, cb) {
+    var sql = ` SELECT * FROM OrderItems WHERE
+                OrderId = ? AND ProductID = ?`;
+
+    var values = [];
+    values.push(data.orderId);
+    values.push(data.productId);
+    sqlConnection.executeQuery(sql, values, function(err, result) {
+        cb(err, result);
+    });
+}
+
+module.exports = {addOrderItem, getOrderItems, editOrderItem, deleteOrderItem};
