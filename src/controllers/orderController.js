14c386a8daa24445c5ffff901af7245081691aaf
singhsanket143 <singhsanket143@gmail.com>
2022-01-10 21:19:47 +0530

    Added better variable names

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -9,28 +9,28 @@ function createOrder(req, res) {
     };
     if(data.userId && data.productId) {
         data.quantity = 1;
-        Product.getProductDetails(data, function(err, result) {
+        Product.getProductDetails(data, function(err, product) {
             if(err) {
                 console.log("error in fetching products",err);
                 responseData.msg = "Error in creating the order";
                 return res.status(500).send(responseData);
             }
-            OrderDetails.findOrderByUser(data, function (err1, result1) {
+            OrderDetails.findOrderByUser(data, function (err1, order) {
                 if(err1) {
                     console.log("error in finding user order",err);
                     responseData.msg = "Error in creating the order";
                     return res.status(500).send(responseData);
                 }
-                if(result1.length > 0) {
-                    data.total = parseInt(result1[0].total, 10) + parseInt(result[0].price, 10);
-                    data.orderId = result1[0].ID;
-                    OrderDetails.editOrder(data, function (err2, result2) {
+                if(order.length > 0) {
+                    data.total = parseInt(order[0].total, 10) + parseInt(product[0].price, 10);
+                    data.orderId = order[0].ID;
+                    OrderDetails.editOrder(data, function (err2, orderDetail) {
                         if(err2) {
                             console.log("error in feditting order",err);
                             responseData.msg = "Error in creating the order";
                             return res.status(500).send(responseData);
                         }
-                        OrderItem.addOrderItem(data, function (err3, result3) {
+                        OrderItem.addOrderItem(data, function (err3, orderItem) {
                             if(err3) {
                                 console.log("error in adding orderitem",err);
                                 responseData.msg = "Error in creating the order";
@@ -39,22 +39,22 @@ function createOrder(req, res) {
                             responseData.msg = "Successfully created an order";
                             responseData.success = true;
                             responseData.orderDetails = {
-                                orderId: result1[0].Id
+                                orderId: order[0].Id
                             }
                             return res.status(200).send(responseData);
                         })
                     });
                 } else {
-                    data.total = parseInt(result[0].price, 10);
-                    OrderDetails.addOrder(data, function(err2, result2) {
+                    data.total = parseInt(product[0].price, 10);
+                    OrderDetails.addOrder(data, function(err2, orderDetail) {
                         if(err2) {
                             console.log("error in adding new order",err);
                             responseData.msg = "Error in creating the order";
                             return res.status(500).send(responseData);
                         }
                         //console.log()
-                        data.orderId = result2.insertId;
-                        OrderItem.addOrderItem(data, function (err3, result3) {
+                        data.orderId = orderDetail.insertId;
+                        OrderItem.addOrderItem(data, function (err3, orderItem) {
                             if(err3) {
                                 console.log("error in adding orderitem",err);
                                 responseData.msg = "Error in creating the order";
@@ -63,7 +63,7 @@ function createOrder(req, res) {
                             responseData.msg = "Successfully created an order";
                             responseData.success = true;
                             responseData.orderDetails = {
-                                orderId: result2.insertId
+                                orderId: orderDetail.insertId
                             }
                             return res.status(200).send(responseData);
                         })
@@ -71,6 +71,8 @@ function createOrder(req, res) {
                 }
             })
         })
+    } else {
+        return res.status(400).send({message: "Invalid data"});
     }
 }
 
