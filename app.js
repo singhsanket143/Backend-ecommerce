206117d718a48dfc1bbc3328321a8051b3509be9
singhsanket143 <singhsanket143@gmail.com>
2022-01-14 22:56:31 +0530

    Added logger

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var morgan = require('morgan');
 var cors = require('cors');
+var winston = require('winston');
+
+
+const logger = winston.createLogger({
+	level: 'info',
+	format: winston.format.json(),
+	transports: [
+		new winston.transports.File({ filename: 'combined.log' }),
+	],
+  });
+
+
+  const apiLogs = function(req, res, next) {
+	  logger.info("API Inputs", {body: req.body});
+	  logger.info("API Path", {path: req.path});
+	  next();
+  }
 
 // var indexRouter = require('./routes/api/v1/index');
 var apiRouter = require('./routes/api');
@@ -14,14 +31,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/api', apiRouter); // whenever we will hit any url starting with / then we will invoke the index router
+app.use('/api', apiLogs, apiRouter); // whenever we will hit any url starting with / then we will invoke the index router
 // app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
